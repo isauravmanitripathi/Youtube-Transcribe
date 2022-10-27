08cecbf9360d311b6ea7539fdac0e49fb08b8319
saurav tripathi <spmtripathi10@gmail.com>
2022-10-27 15:47:41 +0100

    third commit

---
+++ Transcribe.py
@@ -0,0 +1,26 @@
+import urllib
+import xml.etree.ElementTree as ET
+
+def get_transcript(timedtext_url):
+    handle = urllib.urlopen(timedtext_url)
+    contents = handle.read()
+
+
+    root = ET.fromstring(contents)
+
+    running_words = []
+    body = root.find("body")
+    for p in body.findall("p"):
+        if p.text is None or p.text == "\n":
+            words = [clean_text(s.text) for s in p.findall("s")]
+        else:
+            words = [clean_text(p.text)]
+        running_words.extend(words)
+
+    final_text = " ".join(running_words)
+
+    return final_text
+
+
+def clean_text(text):
+    return text.strip().replace("\n", " ")
